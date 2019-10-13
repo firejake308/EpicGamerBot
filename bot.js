92f7fd7960e19e7d35ae55921dea722a2c78f376
Adel Hassan <sean.dashals@gmail.com>
2019-10-13 13:14:46 -0500

    joins and leaves voice channel

--- bot.js
+++ bot.js
@@ -6,12 +6,42 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}`)
+	client.user.setActivity('Chilling like a villain')
 })
 
 client.on('message', msg => {
 	if (msg.content === 'ping') {
 		msg.reply('pong')
 	}
+	if(msg.content.toUpperCase() === '!EGB LINK START') {
+		// attempt to join voice channel of sender
+		if (!msg.guild) return; // ignore if the message wasn't in a server
+		if (msg.member.voiceChannel)
+			startRecording(msg.member.voiceChannel, msg)
+		else {
+			msg.reply('It looks like you\'re not in a voice channel right now')
+		}
+	}
+	if (msg.content.toUpperCase() === '!EGB STOP') {
+		if (msg.member.voiceChannel)
+			stopRecording(msg.member.voiceChannel)
+		else
+			msg.reply('I\'m just chilling bro')
+	}
 })
 
 client.login('***REMOVED***')
+
+// joins channel and updates status
+function startRecording(voiceChannel, msg) {
+	voiceChannel.join().then(cxn => {
+		msg.reply('Initiating epic gaming')
+		client.user.setActivity('Gaming Epically')
+	}).catch(console.log)
+}
+
+// leaves channel and updates status
+function stopRecording(voiceChannel) {
+	voiceChannel.leave()
+	client.user.setActivity('Chilling like a villain')
+}
